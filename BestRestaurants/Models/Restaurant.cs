39c59bd736db0ff24e0c467a4aa9abcb9d7787bf
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-09 10:56:31 -0800

    update Restaurant class

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -4,7 +4,15 @@ namespace BestRestaurants.Models
   {
     public int RestaurantId { get; set; }
     public string Description { get; set; }
+    public string Location { get; set; }
+    public string CostRange { get; set; }
+    public string Rating { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
+
+    // public enum RatingList
+    // {
+    //   One, Two, Three, Four, Five
+    // }
   }
 }
