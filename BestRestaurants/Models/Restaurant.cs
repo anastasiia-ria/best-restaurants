d6166659a30f071152cb46a8fba1cd4b50c7c824
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-10 10:48:10 -0800

    update Models
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -1,18 +1,22 @@
+using System.Collections.Generic;
+
 namespace BestRestaurants.Models
 {
   public class Restaurant
   {
     public int RestaurantId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }
     public string Location { get; set; }
     public string CostRange { get; set; }
     public string Rating { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
 
-    // public enum RatingList
-    // {
-    //   One, Two, Three, Four, Five
-    // }
+    public Restaurant()
+    {
+      this.Reviews = new HashSet<Review>();
+    }
+
+    public virtual ICollection<Review> Reviews { get; set; }
   }
 }
